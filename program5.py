b37c485976f5aa9c7384ef53d246aa029c68a5fb
sidepixel73 <asusgamens@gmail.com>
2023-02-06 10:15:59 +0500

    added print to function

--- program5.py
+++ program5.py
@@ -3,6 +3,7 @@ def converter_to_seven(number):
     digits = '0123456'
     result = ''
     while number > 0:
+        print(result)
         result = digits[number % 7] + result
         number //= 7
     return result
